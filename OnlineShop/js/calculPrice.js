f5c9a73923d344fb1053dd287f8657fb31ace073
alinakamalova204 <alinakamalova919@gmail.com>
2023-12-03 19:37:16 +0300

    реализовано: правильно считает сумму заказа и итоговую сумму с учетом доставки

--- OnlineShop/js/calculPrice.js
+++ OnlineShop/js/calculPrice.js
@@ -1,32 +1,33 @@
 //рассчет цены
 function priceFoodAndDelivery(){
     const cartItems = document.querySelectorAll('.cart-item');
-    let totalPrice = 0;
+    let Price = 0;
+
     cartItems.forEach(function (item) {
         const amountElem = item.querySelector('[data-counter]'); //ищем по аттрибуту <div class="items__current" data-counter="">${productInfo.counter}</div>
         const priceElem = item.querySelector('.price__currency'); //ищем по классу
 
         const currPrice = parseInt(amountElem.innerText) * parseInt(priceElem.innerText);
-        totalPrice += currPrice;
+        Price += currPrice;
 
     })
-    const totalPriceHtml = document.querySelector('.total-price');
-    totalPriceHtml.innerText = totalPrice;
+    //сумма заказа
+    let orderPriceHtml = document.querySelector('.order-price');
+    orderPriceHtml.innerText = Price;
 
+    //показывает или  скрывает блок "корзина пуста"
     const cartDelivery = document.querySelector('[data-cart-delivery]');
-    if (totalPrice > 0){
+    if (orderPriceHtml > 0){
         cartDelivery.classList.remove('none');
     } else {
         cartDelivery.classList.add('none');
     }
 
-    //считаем стоимость доставки
-    const deliveryCost = document.querySelector('.delivery-cost');
-    if (totalPrice >= 600){
-        deliveryCost.classList.add('free');
-        deliveryCost.innerText = 'бесплатно';
+    //считаем итог стоимость доставки + заказ
+    const totalPrice = document.querySelector('.total-price');
+    if (orderPriceHtml.innerText >= 600){
+        totalPrice.innerText = orderPriceHtml.innerText;
     } else {
-        deliveryCost.classList.remove('free');
-        deliveryCost.innerText = '200'
+        totalPrice.innerText = parseInt(orderPriceHtml.innerText) + 200;
     }
 }
